4f2498d29d7066169ff24b05a8b09fb2596aea29
Alex <53095140+CometBlazer@users.noreply.github.com>
2025-02-14 17:41:41 -0500

    feat: add route to greet user by name

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,13 @@ import { Express, Request, Response } from 'express';
 
 const app: Express = express();
 
+// Add route with name parameter
+app.get('/:name', (req: Request, res: Response) => {
+  const name = req.params.name;
+  res.json({ message: `Hello World ${name}` });
+});
+
+// Keep the default route
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello World' });
 });
